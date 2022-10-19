c7fd6006b38d1735a1abf4bfe1c39ac6b79116f8
utkusapaz <utku.sapaz@std.yeditepe.edu.tr>
2022-10-19 03:36:47 +0300

    IQKeyboardManager add

--- FlickrApp/FlickrApp/App/Delegate/AppDelegate.swift
+++ FlickrApp/FlickrApp/App/Delegate/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import CoreData
 import FirebaseCore
 import FirebaseFirestore
+import IQKeyboardManagerSwift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         _ = Firestore.firestore()
+        IQKeyboardManager.shared.enable = true
         return true
     }
 
